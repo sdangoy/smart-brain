3ca84e13d0b89d3b6542be751076f4977bb4872c
Stefan Dangoy <stefan.dangoy@gmail.com>
2020-10-18 13:35:04 -0700

    256 - Created FaceRecognition component to display bounding boxes on faces by manipulating the returned data from Clarifai API.

--- src/App.js
+++ src/App.js
@@ -32,7 +32,27 @@ class App extends Component {
     this.state= {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    console.log(clarifaiFace);
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    //console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -44,15 +64,10 @@ class App extends Component {
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-
-      }
-    )    
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
+
 
   render() {
     return (
@@ -69,7 +84,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
